3c215be31429f9587c65bdfd3e7a41c55a5adb57
Zahnen Garner <zahnen@gmail.com>
2021-03-09 11:19:15 -0800

    add photo upload to new recipe creation

--- client/src/components/NewRecipeForm.js
+++ client/src/components/NewRecipeForm.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "../firebase";
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 function NewRecipeForm(){
   const firestore = firebase.firestore();
   const auth = firebase.auth();
+  const [image, setImage] = useState(null);
+  const [isLoaded, setLoading] = useState(true);
 
   function getItemsFromTextArea(str) {
 		return str.split(",").map(x => x.trim());
 	}
 
-  function addRecipeToFirestore(event) {
+  const handleChange = e => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  async function addRecipeToFirestore(event) {
     event.preventDefault();
+    const imageRef = firebase.storage().ref("images").child(`${image.name}`);
+    imageRef.put(image);
+    const receivedUrl = await imageRef.getDownloadURL().then(setLoading(false));
     firestore.collection('recipes').add(
       {
         title: event.target.title.value,
         author: event.target.author.value,
         ingredients: getItemsFromTextArea(event.target.ingredients.value),
         instructions: getItemsFromTextArea(event.target.instructions.value),
-        // imgURL: url,
+        notes: event.target.notes.value,
+        imgURL: receivedUrl,
         userId: auth.currentUser.uid
       }
-    )
+    );
   }
-  
+
   return (
     <>
       <div className="container" style={{width: '48rem'}}>
@@ -62,10 +75,19 @@ function NewRecipeForm(){
               placeholder="Instructions (separate entries with commas)" />
           </div>
           <div className="mb-3">
-            <label name="image" className="form-label">Default file input example</label>
-            <input className="form-control" type="file" name="image" />
+            <label name="notes" className="form-label">Instructions</label>
+            <textarea
+              className="form-control"
+              type="textarea"
+              name="notes"
+              placeholder="Recipe Notes" />
+          </div>
+          <div className="mb-3">
+            <label name="image" className="form-label">Photo</label>
+            <input className="form-control" type="file" name="image" onChange={handleChange} />
           </div>
           <button variant="success" type="submit">Add Recipe</button>
+          {isLoaded ? <button variant="success" type="submit">Begin Upload</button> : <button variant="success" type="submit">Complete</button>}
         </form>
       </div>
     </>
