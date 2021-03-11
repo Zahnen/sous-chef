94af022fb086f875fc0d173e7fd6ce0efc4e02f4
Zahnen Garner <zahnen@gmail.com>
2021-03-10 22:59:53 -0800

    add logo, reorder links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './../img/SousChefLogo.svg'
 
 function NavBar() {
 return(
   <>
   <nav className="navbar navbar-expand-lg navbar-light bg-white">
     <div className="container-fluid">
-      <Link to="/" className="navbar-brand" >Sous Chef</Link>
+      <Link to="/" className="navbar-brand" ><img src={Logo} alt="Sous Chef Logo"  width="250px"></img></Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/myrecipes">My Recipes</Link>
+          </li>
+          <li className="nav-item">
             <Link className="nav-link active" aria-current="page" to="/addrecipeurl">Add Recipe Via URL</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link active" aria-current="page" to="/addrecipe">Add Recipe Manually</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/myrecipes">My Recipes</Link>
           </li>
         </ul>
         <div className="collapse navbar-collapse text-right navbarNav">
