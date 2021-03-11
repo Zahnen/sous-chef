f1248d57640a0c2e895be3505736daa8fcf0ec64
Zahnen Garner <zahnen@gmail.com>
2021-03-10 22:57:29 -0800

    add text to Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Home(){
   return (
     <>
-      <h1>Hello.</h1>
+      <div className="container" style={{marginTop: '15%', paddingBottom: '50px', width: '36rem'}}>
+        <h1>Sous Chef simplifies the art of making food.</h1>
+        <p>Sous Chef is a straightforward recipe manager that helps declutter online recipes & simplify the process of keeping them. </p>
+      </div>
     </>
   )
 }
