3e7a086c0db3ebff9f9c9feee322bc50b72c571a
Zahnen Garner <zahnen@gmail.com>
2021-03-10 22:57:01 -0800

    add Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavBar() {
+return(
+  <>
+    <div className="footer">
+      <p>Baked with love by <a style={{color: "black"}} href="https://github.com/Zahnen" target="_blank" rel="noopener noreferrer">@Zahnen</a></p>
+    </div>
+  </>
+)}
+
+export default NavBar;
